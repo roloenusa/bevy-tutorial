f79273221a0ad2a3ddc546bdaa675749fbd6ac5b
Juan Delgado <juan.delgado@fortisgames.com>
2025-10-06 16:14:28 -0700

    Positioning Platforms

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
 use bevy::color::palettes::css::LIMEGREEN;
+use bevy::window::WindowResolution;
+
+const WINDOW_WIDTH: f32 = 1024.0;
+const WINDOW_HEIGHT: f32 = 720.0;
+
+const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
+const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Platformer".to_string(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
@@ -14,12 +29,36 @@ fn setup(mut commands: Commands) {
     commands.spawn((
         Sprite {
             color: LIMEGREEN.into(),
-            ..Default::default()
+            ..default()
         },
         Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            scale: Vec3::new(50.0, 100.0, 1.0),
-            ..Default::default()
+            translation: Vec3::new(-100.0, WINDOW_BOTTOM_Y + (200.0 / 2.0), 0.0),
+            scale: Vec3::new(75.0, 200.0, 1.0),
+            ..default()
         },
+    ));
+
+    commands.spawn((
+        Sprite {
+            color: LIMEGREEN.into(),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(100.0, WINDOW_BOTTOM_Y + (350.0 / 2.0), 0.0),
+            scale: Vec3::new(50.0, 350.0, 1.0),
+            ..default()
+        }
+    ));
+
+    commands.spawn((
+        Sprite {
+            color: LIMEGREEN.into(),
+            ..default()
+        },
+        Transform {
+            translation: Vec3::new(350.0, WINDOW_BOTTOM_Y + (250.0 / 2.0), 0.0),
+            scale: Vec3::new(150.0, 250.0, 1.0),
+            ..default()
+        }
     ));
 }
